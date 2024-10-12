24c24df991acf8265a4a2ffdfca575e4698c26e0
Michal0808 <aug.mic.10@wp.pl>
2024-10-12 18:21:58 +0200

    do domu

--- test.java
+++ test.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class test {
-    public static void main(String[] args){
-        System.out.println("Hello World test");
-    }
+    public static void main(String[] args) {
+        System.out.println("Witam, jestem programem sprawdzającym parzystość liczby");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+
+    int inputNumber1 = scanner.nextInt();
+    scanner.close();
+
+    if()
+
+    System.out.println("Podana liczba to:" + inputNumber);
+  }
 }
