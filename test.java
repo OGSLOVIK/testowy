6a7acbc94213475787177498c722ae3d00f7de8a
wa_pslowikowski <pslowik2@icloud.com>
2024-10-17 16:59:57 +0200

    koniec roboty, fajrant

--- test.java
+++ test.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-public class klasa {
+public class test {
     public static void main(String[] args) {
-        System.out.println("Witam w kutasowie");
+        System.out.println("Witam w programie");
 
         Scanner scaner = new Scanner(System.in);
         System.out.println("Podaj liczbę:");
@@ -15,6 +15,7 @@ public class klasa {
             System.out.println("Podana liczba to:" +inputNumber + " i jest parzysta");
             } else {
             System.out.println("Podana liczba to:" +inputNumber + " i jest nieparzysta");
+            System.out.println("Jebac kramka");
         }
     }
 }
