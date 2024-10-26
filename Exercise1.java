3e44ac07fbf6a3391fc073a8cad8e2348ab3d7a6
Student <thousandroundss@gmail.com>
2024-10-26 15:42:37 +0200

    Konwerter

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Witam w konwerterze temperatury");
+        System.out.println("Podaj temperaturę w *C : ");
+        float value = userInput.nextInt();
+        double Fahrenheit = 1.8;
+
+        double wynik = (Fahrenheit * value + 32.0 );
+        System.out.println("Temperature w Fahrenheit to : " +wynik);
+
+    }
+}
